892f367c0e4894077a8cbb31960aa635fbd1276b
Yong In <brin7584@gmail.com>
2022-09-27 09:19:38 +0900

    feat: insert func function: n번째 node 뒤로 node 삽입

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -31,6 +31,25 @@ class LinkedList<T> {
         size++
         return this
     }
+    fun nodeAt(index: Int): Node<T>? {
+        var currentNode = head
+        var currentIndex = 0
 
+        while (currentNode != null && currentIndex < index) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode
+    }
+    fun insert(value: T, afterNode:Node<T>):Node<T> {
+        if (tail == afterNode) {
+            append(value)
+            return tail!!
+        }
 
+        val newNode = Node(value=value, next = afterNode.next)
+        afterNode.next = newNode
+        size++
+        return newNode
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,13 @@ fun main() {
     var list = LinkedList<Int>()
     list.push(value=10).push(value = 20).push(value=40).push(value=50).push(value=60).push(value=70)
     list.append(value = 6).append(value = 5).append(value = 4).append(value = 3).append(value = 2).append(value = 1)
-    println(list)
+    println("Before insertng: $list")
+
+    var middleNode = list.nodeAt(5)!!
+    for(i in 1..3){
+        middleNode = list.insert((10-i), middleNode)
+    }
+    println("After insertng: $list")
 }
 
 
