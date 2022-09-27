98975ab1d223c89ec59b8d9b80d7b040cfa768a0
Yong In <brin7584@gmail.com>
2022-09-27 09:28:04 +0900

    feat: removeLast func function: remove tail node and return tail node value

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -61,5 +61,21 @@ class LinkedList<T> {
         }
         return result
     }
+    fun removeLast(): T? {
+        val head = head ?: return null
+        if(head.next==null)return pop()
+        size--
+        var prev = head
+        var current = head
+        var next=current.next
+        while(next != null){
+            prev = current
+            current = next
+            next = current.next
+        }
+        prev.next = null
+        tail = prev
+        return current.value
+    }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,13 @@
 
 fun main() {
     var list = LinkedList<Int>()
-    var poppedValue = list.pop()
-    println(poppedValue)
-
     list.push(value=10).push(value = 20).push(value=40).push(value=50).push(value=60).push(value=70)
 
-    println("Before popping list: $list")
-    poppedValue=list.pop()
-    println("After popping list: $list")
-    println("Popped Value: $poppedValue")
+    println("Before removing last node: $list")
+    val removedValue = list.removeLast()
+
+    println("After removing last node: $list")
+    println("Removed Value: $removedValue")
 
 }
 
