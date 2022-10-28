3e76758d2f5635a0afaaa985194abbcde93ebcfe
poeo1877 <brin7584@gmail.com>
2022-10-28 12:12:05 +0900

    Heap implement Heap을 추상화하여 compare() 구현 방식에 따라 minheap or maxheap이 되도록 class 구현

---
+++ src/main/kotlin/Heap.kt
@@ -0,0 +1,122 @@
+import java.util.Collections
+
+interface MyCollection<Element> {
+    val count: Int
+        get
+    val isEmpty: Boolean
+        get() = count == 0
+    fun insert(element: Element)
+    fun remove(): Element?
+    fun remove(index: Int): Element?
+}
+
+interface HeapInterface<Element> : MyCollection<Element> {
+    fun peek(): Element?    //node의 값을 볼 수 있게 하는 함수
+}
+
+abstract class AbstractHeap<Element>(): HeapInterface<Element>{
+    //max or min Heap 무관하게 공통인 것 정의...
+    var elements: ArrayList<Element> = ArrayList<Element>()
+    override val count: Int
+        get() = elements.size //elements의 item 개수를 가져옴 즉, tree에 있는 모든 노드 개수
+
+    override fun peek():Element? = elements.first()  //root node의 값을 보게 override
+    private fun leftChildIndex(index: Int) = (2*index)+1
+    private fun rightChildIndex(index: Int) = (2*index)+2
+    private fun parentIndex(index: Int)= (index-1)/2
+
+    abstract fun compare(a: Element, b: Element) : Int //이 함수를 어떻게 구현하는지에 따라 Maxheap or Minheap으로 갈린다
+
+    private fun siftUp(index: Int){
+        var child = index
+        var parent = parentIndex(child)
+        while (child > 0 && compare(elements[child], elements[parent])>0){
+            Collections.swap(elements, child,parent) //import java.util.Collections가 추가돼 사용 가능한 코드
+            child = parent
+            parent = parentIndex(child)
+        }
+    }
+
+    override fun insert(element: Element){
+        elements.add(element)
+        siftUp(count-1)
+    }
+
+    private fun siftDown(index:Int){
+        var parent = index
+        while(true){
+            val left = leftChildIndex(parent)
+            val right = rightChildIndex(parent)
+            var candidate = parent
+            if (left < count &&
+                compare(elements[left], elements[candidate]) > 0) {
+                candidate = left
+            }
+            if (right < count &&
+                compare(elements[right], elements[candidate]) > 0) {
+                candidate = right
+            }
+            if(candidate == parent)
+                return
+            Collections.swap(elements, parent, candidate)
+            parent = candidate
+        }
+    }
+    override fun remove():Element? {
+        if(isEmpty) return null
+        Collections.swap(elements, 0, count - 1)
+        val item = elements.removeAt(count - 1)
+        siftDown(0)
+        return item
+    }
+    override fun remove(index : Int): Element? {
+        if (index >= count) return null
+        //조건에 따라 return 대상(각 code block에서 가장 마지막 줄)이 달라진다...
+        return if (index == count - 1) {
+            elements.removeAt(count - 1)
+        } else {
+            Collections.swap(elements, 0, count - 1)
+            val item = elements.removeAt(count - 1)
+            siftDown(index)
+            siftUp(index)
+            item //heap에서 root node 값을 반환해주는 것
+        }
+    }
+
+    private fun index(element: Element, i: Int): Int?{
+        /// i는 재귀호출을 위한 값
+        if(i>= count)
+            return null
+        if(compare(element, elements[i])>0)
+            return null
+        if(element == elements[i])
+            return i
+
+        var leftChildIndex = index(element, leftChildIndex(i))
+        if(leftChildIndex != null) return leftChildIndex
+        val rightChildIndex = index(element, rightChildIndex(i))
+        if(rightChildIndex != null)return rightChildIndex
+        return null
+    }
+
+    //임의의 배열을 heap으로 만드는 것
+    fun heapify(values: ArrayList<Element>){
+        elements = values
+        if(!elements.isEmpty()){
+            (count/2 downTo 0).forEach{
+                siftDown(it)
+            }
+        }
+    }
+}
+
+class MaxHeap<Element: Comparable<Element>>() : AbstractHeap<Element>() {
+    override fun compare(a: Element, b: Element): kotlin.Int{
+        return a.compareTo(b)
+    }
+}
+class MinHeap<Element: Comparable<Element>>() : AbstractHeap<Element>() {
+    override fun compare(a: Element, b: Element): kotlin.Int{
+        return b.compareTo(a)
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,10 @@
-fun makeBeverageTree():TreeNode<String>{
-    val tree = TreeNode("Beverages")
-    val hot = TreeNode("hot")
-    val cold = TreeNode("cold")
-    val tea = TreeNode("tea")
-    val coffee = TreeNode("coffee")
-    val chocolate = TreeNode("cocoa")
-    val blackTea = TreeNode("black")
-    val greenTea = TreeNode("green")
-    val chaiTea = TreeNode("chai")
-    val soda = TreeNode("soda")
-    val milk = TreeNode("milk")
-    val gingerAle = TreeNode("ginger ale")
-    val bitterLemon = TreeNode("bitter lemon")
-    tree.add(hot)
-    tree.add(cold)
-    hot.add(tea)
-    hot.add(coffee)
-    hot.add(chocolate)
-    cold.add(soda)
-    cold.add(milk)
-    tea.add(blackTea)
-    tea.add(greenTea)
-    tea.add(chaiTea)
-    soda.add(gingerAle)
-    soda.add(bitterLemon)
-    return tree
-}
-
-fun makeBinaryTree(): BinaryNode<Int>{
-    val zero = BinaryNode(0)
-    val one = BinaryNode(1)
-    val five = BinaryNode(5)
-    val seven = BinaryNode(7)
-    val eight = BinaryNode(8)
-    val nine = BinaryNode(9)
-    seven.leftChild = one
-    one.leftChild = zero
-    one.rightChild = five
-    seven.rightChild = nine
-    nine.leftChild = eight
-    val tree = seven
-    return tree
-}
 
 
 fun main(){
-    val array = arrayListOf(1,5,15,17,19,22,24,31,105,150)
-    val search31 = array.indexOf(31)
-    val binarySearch31 = array.binarySearch(31)
-    println("indexOf(): $search31")
-    println("binarySearch(): $binarySearch31")
-    println("--------------------------------")
-    val arr = arrayListOf(1,2,3,3,3,4,5,5)
-    val indices = arr.findIndices(3)
-    println(indices)
+    val array = arrayListOf(1,12,3,4,1,6,8,7)
+    val h = MinHeap<Int>()
+    h.heapify(array)
+    while(!h.isEmpty){
+        println(h.remove())
+    }
 }
