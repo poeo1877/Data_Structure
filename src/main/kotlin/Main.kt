538aae05651663de3d4637c9f83ee195744bbf61
poeo1877 <brin7584@gmail.com>
2022-11-18 12:26:26 +0900

    Heap sort_descending order main.kt에 codeline_3 수정...

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(){
     val array = arrayOf(6, 12, 2, 26, 8, 18, 21, 9, 5)
-    array.heapSort(Comparator{o1: Int, o2:Int -> o1 - o2})
+    array.heapSort(Comparator{o1: Int, o2:Int -> o2 - o1})  //내림차순으로 정렬...
     print(array.joinToString())
 }
