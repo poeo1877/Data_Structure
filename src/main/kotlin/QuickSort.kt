d14e973a02c4fccca1bd0cb1799a578a0cf35b01
poeo1877 <brin7584@gmail.com>
2022-11-18 15:36:40 +0900

    Quick sort_using loop_Lomuto Algorithm 재귀호출 없이 반복문을 사용해 Lomuto 알고리즘 구현. Stack을 이용해 구현함.

--- src/main/kotlin/QuickSort.kt
+++ src/main/kotlin/QuickSort.kt
@@ -47,3 +47,50 @@ fun<T: Comparable<T>> MutableList<T>.quicksortHoare(low: Int, high: Int){
         this.quicksortHoare(p+1, high)
     }
 }
+
+fun<T: Comparable<T>> MutableList<T>.partitionDutchFlag(low:Int, high:Int, pivotIndex: Int): Pair<Int, Int>{
+    val pivot = this[pivotIndex]
+    var smaller = low
+    var equal = low
+    var larger = high
+    while(equal <= larger) {
+        if(this[equal] < pivot) {
+            this.swapAt(smaller, equal)
+            smaller += 1
+            equal += 1
+        } else if(this[equal] == pivot) {
+            equal += 1
+        } else {
+            this.swapAt(equal, larger)
+            larger -= 1
+        }
+    }
+    return Pair(smaller, larger)
+}
+
+fun<T: Comparable<T>> MutableList<T>.quicksortDutchFlag(low:Int, high:Int) {
+    if(low < high){
+        val middle = partitionDutchFlag(low, high, high)
+        this.quicksortDutchFlag(low, middle.first -1)
+        this.quicksortDutchFlag(middle.second + 1 , high)
+    }
+}
+
+fun<T: Comparable<T>> MutableList<T>.quicksortIterativeLomuto(low: Int, high: Int){
+    val stack = Stack<Int>()
+    stack.push(low)
+    stack.push(high)
+    while (!stack.isEmpty){
+        val end= stack.pop()?:continue
+        val start = stack.pop() ?: continue
+        val p = this.partitionLomuto(start, end)
+        if((p-1) > start){  //왼쪽에 더 conquer할 것이 있는가?
+            stack.push(start)
+            stack.push(p-1)
+        }
+        if((p+1) < end){
+            stack.push(p+1)
+            stack.push(end)
+        }
+    }
+}
