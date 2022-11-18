04e37f48b0f4dcddbdc8c079335363533ac797c5
poeo1877 <brin7584@gmail.com>
2022-11-18 14:20:29 +0900

    Quick sort_Hoare's Algorithm

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(){
     val list = arrayListOf(12, 0, 3, 9, 2, 21, 18, 27, 1, 5, 8, -1, 8)
     println("Original: $list")
-    list.quicksortLomuto(0, list.size-1)
+    list.quicksortHoare(0, list.size-1)
     println("Sorted: $list")
 
 }
--- src/main/kotlin/QuickSort.kt
+++ src/main/kotlin/QuickSort.kt
@@ -20,3 +20,30 @@ fun<T: Comparable<T>> MutableList<T>.quicksortLomuto(low: Int, high: Int){
         this.quicksortLomuto(pivot+1, high)
     }
 }
+
+fun<T: Comparable<T>> MutableList<T>.partitionHoare(low: Int, high: Int):Int{
+    val pivot = this[low]
+    var i = low -1      //do-while에서 일단 1을 감소하기에 값을 조정...
+    var j = high + 1
+    while(true){
+        do{
+            j -= 1
+        }while(this[j] > pivot)
+        do{
+            i += 1
+        }while(this[i] < pivot)
+        if(i<j){
+            this.swapAt(i, j)
+        } else{
+            return j
+        }
+    }
+}
+
+fun<T: Comparable<T>> MutableList<T>.quicksortHoare(low: Int, high: Int){
+    if(low < high) {
+        val p = this.partitionHoare(low, high)
+        this.quicksortHoare(low, p)
+        this.quicksortHoare(p+1, high)
+    }
+}
